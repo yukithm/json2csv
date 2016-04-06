c81692c423602efa82848216155202d5eb486faf
Yuki <yukithm@gmail.com>
2016-04-06 11:34:00 +0900

    Add --transpose option
    --transpose option transpose rows and columns.

--- csv_writer.go
+++ csv_writer.go
@@ -20,7 +20,8 @@ const (
 // CSVWriter writes CSV data.
 type CSVWriter struct {
 	*csv.Writer
-	headerStyle keyStyle
+	HeaderStyle keyStyle
+	Transpose   bool
 }
 
 // NewCSVWriter returns new CSVWriter with JSONPointerStyle.
@@ -28,29 +29,27 @@ func NewCSVWriter(w io.Writer) *CSVWriter {
 	return &CSVWriter{
 		csv.NewWriter(w),
 		JSONPointerStyle,
+		false,
 	}
 }
 
 // WriteCSV writes CSV data.
 func (w *CSVWriter) WriteCSV(results []keyValue) error {
+	if w.Transpose {
+		return w.writeTransposedCSV(results)
+	}
+	return w.writeCSV(results)
+}
+
+// WriteCSV writes CSV data.
+func (w *CSVWriter) writeCSV(results []keyValue) error {
 	pts, err := allPointers(results)
 	if err != nil {
 		return err
 	}
 	sort.Sort(pts)
 	keys := pts.Strings()
-
-	var header []string
-	switch w.headerStyle {
-	case JSONPointerStyle:
-		header = keys
-	case SlashStyle:
-		header = pts.Slashes()
-	case DotNotationStyle:
-		header = pts.DotNotations(false)
-	case DotBracketStyle:
-		header = pts.DotNotations(true)
-	}
+	header := w.getHeader(pts)
 
 	if err := w.Write(header); err != nil {
 		return err
@@ -58,6 +57,31 @@ func (w *CSVWriter) WriteCSV(results []keyValue) error {
 
 	for _, result := range results {
 		record := toRecord(result, keys)
+		if err := w.Write(record); err != nil {
+			return err
+		}
+	}
+
+	w.Flush()
+	if err := w.Error(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// WriteCSV writes CSV data which is transposed rows and columns.
+func (w *CSVWriter) writeTransposedCSV(results []keyValue) error {
+	pts, err := allPointers(results)
+	if err != nil {
+		return err
+	}
+	sort.Sort(pts)
+	keys := pts.Strings()
+	header := w.getHeader(pts)
+
+	for i, key := range keys {
+		record := toTransposedRecord(results, key, header[i])
 		if err := w.Write(record); err != nil {
 			return err
 		}
@@ -88,10 +112,38 @@ func allPointers(results []keyValue) (pointers pointers, err error) {
 	return
 }
 
+func (w *CSVWriter) getHeader(pointers pointers) []string {
+	switch w.HeaderStyle {
+	case JSONPointerStyle:
+		return pointers.Strings()
+	case SlashStyle:
+		return pointers.Slashes()
+	case DotNotationStyle:
+		return pointers.DotNotations(false)
+	case DotBracketStyle:
+		return pointers.DotNotations(true)
+	default:
+		return pointers.Strings()
+	}
+}
+
 func toRecord(kv keyValue, keys []string) []string {
 	record := make([]string, 0, len(keys))
 	for _, key := range keys {
 		if value, ok := kv[key]; ok {
+			record = append(record, toString(value))
+		} else {
+			record = append(record, "")
+		}
+	}
+	return record
+}
+
+func toTransposedRecord(results []keyValue, key string, header string) []string {
+	record := make([]string, 0, len(results)+1)
+	record = append(record, header)
+	for _, result := range results {
+		if value, ok := result[key]; ok {
 			record = append(record, toString(value))
 		} else {
 			record = append(record, "")
